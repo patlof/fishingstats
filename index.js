9c0d1e871b566f51849ec14b6a6266a13f3b98e0
patlof <patrik.lofvander@yahoo.se>
2026-03-31 13:30:30 +0200

    Set up start page 'Intro'

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 import express from "express";
 import Database from "better-sqlite3";
+
+const app = express();
+const port = 4000;
+app.use(express.static("public"));
+//Connect to db
+const db = new Database("data.db");
+
+//Read from database
+const data = db.prepare("SELECT * FROM fish").all();
+
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
+
+// Listening to port 4000
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
